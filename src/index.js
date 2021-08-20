3f6acd2a36687735790085ce101791b7a2821202
Vera Vlasova <vlasovavery@gmail.com>
2021-08-20 17:58:45 +0500

    feat(general logic of the game):will move the general logic to a file index.js
    https://ru.hexlet.io/projects/44/members/15852?step=6

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+export const MAX_NUMBER_ROUNDS = 3;
+
+// eslint-disable-next-line import/no-mutable-exports
+let nameUser = '';
+
+export const promptsForUsername = () => {
+  const name = readlineSync.question('May I have your name?');
+  return name;
+};
+
+export const greetsUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = promptsForUsername();
+  nameUser = name;
+  const greeting = console.log(`Hello, ${name}!`);
+
+  return greeting;
+};
+
+export const willShowRulesGame = (value) => {
+  console.log(value);
+};
+
+export const generatesNumber = () => {
+  const MAX_NUMBER = 100;
+  const generatedNumber = Math.ceil(Math.random() * MAX_NUMBER);
+
+  return generatedNumber;
+};
+
+export { nameUser };
